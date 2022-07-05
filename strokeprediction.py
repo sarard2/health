6e2ddd828e185831c4e2d9ffcea6a8607b61698e
sarard2 <99599777+sarard2@users.noreply.github.com>
2022-07-06 01:20:04 +0300

    Update strokeprediction.py

--- strokeprediction.py
+++ strokeprediction.py
@@ -117,13 +117,13 @@ if selected=="Visuals":
     with col1:
          strokeages=pd.read_csv("allages.csv")
          country_options=strokeages["Location"].unique().tolist()
-         metric_options=strokeages["Metric"].unique().tolist()
+         #metric_options=strokeages["Metric"].unique().tolist()
          my_expander1 = st.expander(label='Press to Edit the Metric!')
          with my_expander1:
                 country_select=st.selectbox('In which country do you want to trace the number of deaths by stroke?',country_options)
-                metric_select=st.selectbox("Which metric are you more intesrested in?",metric_options)
+                #metric_select=st.selectbox("Which metric are you more intesrested in?",metric_options)
                 #number_stroke=strokeages[strokeages["Metric"]=="Number"]
-         filter_dff=strokeages[strokeages["Location"].isin(country_select)&strokeages[strokeages["Metric"].isin(metric_select)]
+         filter_dff=strokeages[strokeages["Location"]==country_select]
 
          figure8=px.area(filter_dff, x='Year', y="val",color="Sex",title="Deaths by Stroke Through Time According to Gender",
          width=400, height=400,
