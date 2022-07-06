72916b29e55731feffb71145a0d17b8d40bd8263
sarard2 <99599777+sarard2@users.noreply.github.com>
2022-07-06 10:38:38 +0300

    Update strokeprediction.py

--- strokeprediction.py
+++ strokeprediction.py
@@ -240,7 +240,15 @@ if selected=="Exploration":
 
 #Prediction Page
 if selected =="Prediction":
-#Logistic Regression Model to predict approximate price of tickets based on variables that the user inputs
+
+    
+ col11,col12=st.columns(3)
+    with col11:
+        st.header("Check Your Health Status!")
+        st.write("Now is your time to predict whether you are subjected to stroke in the near future")
+     with col12:
+        st.write("Try the below now")
+ st.markdown("""<hr style="height:15px;border:none;color:#00ced1;background-color:#00ced1;" /> """, unsafe_allow_html=True)
  col1,col2,col3=st.columns(3)
  with col1:
      #Gender
