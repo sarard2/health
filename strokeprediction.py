41121fdeb56217da58942e02138562b41a25caac
sarard2 <99599777+sarard2@users.noreply.github.com>
2022-07-06 10:32:34 +0300

    Update strokeprediction.py

--- strokeprediction.py
+++ strokeprediction.py
@@ -93,9 +93,10 @@ yearly['Country_Code']=alpha3code(yearly.location)
 
 #Home Page
 if selected =="Home":
-    st.write("hi")
-    st.markdown("""<hr style="height:5px;border:none;color:#00ced1;background-color:#00ced1;" /> """, unsafe_allow_html=True)
-
+    col1,col2,col3:st.columns(3)
+        with col1:
+            st.write("hi")
+            st.markdown("""<hr style="height:5px;border:none;color:#00ced1;background-color:#00ced1;" /> """, unsafe_allow_html=True)
 #Overview page
 if selected=="Data":
 
